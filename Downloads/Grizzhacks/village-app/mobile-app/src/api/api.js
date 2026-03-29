24e828ed48d407ee7dacb1e386348a7aae8c61b3
AryaMani2021 <162825876+AryasreeManimurugan@users.noreply.github.com>
2026-03-29 09:15:19 -0400

    Latest Chat Changes

--- Downloads/Grizzhacks/village-app/mobile-app/src/api/api.js
+++ Downloads/Grizzhacks/village-app/mobile-app/src/api/api.js
@@ -154,6 +154,14 @@ export const sendMessage = async (chat_id, sender_id, content) => {
   return res.json()
 }
 
+
+
+
+export const getChatMembers = async (chat_id) => {
+  const res = await fetch(`${BASE_URL}/api/chats/${chat_id}/members`, { headers });
+  return res.json();
+};
+
 // ── Donations ─────────────────────────────────────────────────────────────────
 
 export const getDonations = async () => {
